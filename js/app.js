638cc76c05d70de010a3b8ff3b32322ad117756b
chrisalexander <chris.alexander@import.io>
2014-03-21 14:14:41 +0000

    Magic happens

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 importio.init(ioconfig);
 var io = angular.module("io", ["importicles"]);
 
-var website = io.controller("Website", ["$scope", "safeApply", "importicleOauthState",
-	function($scope, safeApply, state) {
+var website = io.controller("Website", ["$scope", "safeApply", "importicleOauthState", "cacookie",
+	function($scope, safeApply, state, cookie) {
 
 	// login, list, link, recommend
 	$scope.page = "login";
@@ -10,29 +10,55 @@ var website = io.controller("Website", ["$scope", "safeApply", "importicleOauthS
 	// Current user
 	$scope.user = false;
 
+	// Whether paypal has been initialised
+	$scope.paypalInitialised = false;
+
 	// Ew ew ew
 	$scope.paypal = function() {
 		paypal.use( ["login"], function(login) {
-			login.render ({
-				"appid": "AVti7hDMEIMf2rIWCOw3IqPnf5uk7-WD5YqTbHcli9Ph-Dpucy3wWA8J7w5u",
-				"authend": "sandbox",
-				"scopes": "openid",
-				"containerid": "loginWithPaypal",
-				"locale": "en-gb",
-				"theme": "neutral",
-				"returnurl": "http://pp.chris-alexander.co.uk/who.php"
-			});
+			if (!$scope.paypalInitialised) {
+				$scope.paypalInitialised = true;
+				login.render ({
+					"appid": "AVti7hDMEIMf2rIWCOw3IqPnf5uk7-WD5YqTbHcli9Ph-Dpucy3wWA8J7w5u",
+					"authend": "sandbox",
+					"scopes": "openid",
+					"containerid": "loginWithPaypal",
+					"locale": "en-gb",
+					"theme": "neutral",
+					"returnurl": "http://pp.chris-alexander.co.uk/who.php"
+				});
+			}
 			state.await(function(d, done) {
 				if (d.hasOwnProperty("code") && d.code) {
 					$scope.user = d.code;
-					$scope.page = "list";
+					$scope.page = "link";
+					cookie.create("user", d.code, 1000*60*60);
 				}
 				done();
 				safeApply($scope);
 			});
 		});
 	}
-	setTimeout($scope.paypal, 1000);
+
+	$scope.init = function() {
+		var c = cookie.read("user");
+		if (c) {
+			$scope.page = "list";
+			$scope.user = c;
+			safeApply($scope);
+			return;
+		} else {
+			setTimeout($scope.paypal, 1000);
+		}
+	}
+	$scope.init();
+
+	$scope.logout = function() {
+		cookie.create("user", false, -1);
+		$scope.user = false;
+		$scope.page = "login";
+		$scope.paypal();
+	}
 
 }]);
 
